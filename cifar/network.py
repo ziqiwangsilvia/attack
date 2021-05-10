fix cuda problem in marco softmax

cifar/network.py
@@ -42,7 +42,7 @@ class marco_softmax(nn.Module):
     def __init__(self, num_classes):
         super(marco_softmax, self).__init__()
         self.num_classes = num_classes
-        self.e = torch.eye(num_classes)
+        self.e = torch.eye(num_classes).cuda()
     def forward(self, input):        
         nu = []
         pos = []