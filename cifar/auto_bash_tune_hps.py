55e89843a3d351fa2f93bfa2b204135329fe58ec
ziqiwangsilvia <wangziqisilvia@hotmail.com>
2021-08-04 14:34:11 +0200

    prepare experiments for cifar10 with resnet18

--- cifar/auto_bash_tune_hps.py
+++ cifar/auto_bash_tune_hps.py
@@ -10,7 +10,7 @@ from utils import check_mkdir
 import os
 import numpy as np
 
-path = 'jobs/tune_hps_vgg16_marco/'
+path = 'jobs/tune_hps_resnet18_False/'
 check_mkdir(path)
 
 conservative = 'marco'
@@ -36,7 +36,7 @@ module use /opt/insy/modulefiles
 module load cuda/10.1 cudnn/10.1-7.6.0.64
 
 echo "Starting at $(date)"
-srun python cifar.py --tune_hps=True --network=resnet18 --conservative=marco""" + ' --lr=' + str(lr)  \
+srun python cifar.py --tune_hps=True --network=resnet18 --conservative=False""" + ' --lr=' + str(lr)  \
     + ' --train_batch_size=' + str(tb) +' --weight_decay=' + str(wd) +'\n' +
 """echo "Finished at $(date)"
 """
