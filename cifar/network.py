e8804f1e966dc9be18dd9e2d5079866a2adf468f
ziqiwangsilvia <wangziqisilvia@hotmail.com>
2021-09-23 13:43:53 +0200

    debug

--- cifar/network.py
+++ cifar/network.py
@@ -44,7 +44,7 @@ class marco_softmax(nn.Module):
     def __init__(self, num_classes):
         super(marco_softmax, self).__init__()
         self.num_classes = num_classes
-        self.e = torch.eye(num_classes)
+        self.e = torch.eye(num_classes).to(device)
     def forward(self, input):        
         nu = []
         pos = []
